b2ac9c72f965c80dc376b6cea689ff399e93f9ac
David A. Ventimiglia <dventimi@gmail.com>
2014-10-17 03:53:57 -0700

    Adapted into personal coding style and then got it to compile.

--- src/main/java/org/atomicframework/filterwheel/GZIPResponseStream.java
+++ src/main/java/org/atomicframework/filterwheel/GZIPResponseStream.java
@@ -1,15 +1,7 @@
-/*
- * Copyright 2003 Jayson Falkner (jayson@jspinsider.com)
- * This code is from "Servlets and JavaServer pages; the J2EE Web Tier",
- * http://www.jspbook.com. You may freely use the code both commercially
- * and non-commercially. If you like the code, please pick up a copy of
- * the book and help support the authors, development of more free code,
- * and the JSP/Servlet/J2EE community.
- */
-package com.jspbook;
+package org.atomicframework.filterwheel;
 
 import java.io.*;
-import java.util.zip.GZIPOutputStream;
+import java.util.zip.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -20,64 +12,46 @@ public class GZIPResponseStream extends ServletOutputStream {
     protected HttpServletResponse response = null;
     protected ServletOutputStream output = null;
 
-    public GZIPResponseStream(HttpServletResponse response) throws IOException {
+    public GZIPResponseStream (HttpServletResponse response) throws IOException {
         super();
         closed = false;
         this.response = response;
         this.output = response.getOutputStream();
         baos = new ByteArrayOutputStream();
-        gzipstream = new GZIPOutputStream(baos);
-    }
+        gzipstream = new GZIPOutputStream(baos);}
 
-    public void close() throws IOException {
-        if (closed) {
-            throw new IOException("This output stream has already been closed");
-        }
+    public void close () throws IOException {
+        if (closed) throw new IOException("This output stream has already been closed");
         gzipstream.finish();
-
         byte[] bytes = baos.toByteArray();
-
-
-        response.addHeader("Content-Length", 
-                           Integer.toString(bytes.length)); 
+        response.addHeader("Content-Length", Integer.toString(bytes.length)); 
         response.addHeader("Content-Encoding", "gzip");
         output.write(bytes);
         output.flush();
         output.close();
-        closed = true;
-    }
+        closed = true;}
 
-    public void flush() throws IOException {
-        if (closed) {
-            throw new IOException("Cannot flush a closed output stream");
-        }
-        gzipstream.flush();
-    }
+    public void flush () throws IOException {
+        if (closed) throw new IOException("Cannot flush a closed output stream");
+        gzipstream.flush();}
 
-    public void write(int b) throws IOException {
-        if (closed) {
-            throw new IOException("Cannot write to a closed output stream");
-        }
-        gzipstream.write((byte)b);
-    }
+    public void write (int b) throws IOException {
+        if (closed) throw new IOException("Cannot write to a closed output stream");
+        gzipstream.write((byte)b);}
 
-    public void write(byte b[]) throws IOException {
-        write(b, 0, b.length);
-    }
+    public void write (byte b[]) throws IOException {
+        write(b, 0, b.length);}
 
-    public void write(byte b[], int off, int len) throws IOException {
-        System.out.println("writing...");
-        if (closed) {
-            throw new IOException("Cannot write to a closed output stream");
-        }
-        gzipstream.write(b, off, len);
-    }
+    public void write (byte b[], int off, int len) throws IOException {
+        if (closed) throw new IOException("Cannot write to a closed output stream");
+        gzipstream.write(b, off, len);}
 
-    public boolean closed() {
-        return (this.closed);
-    }
+    public boolean closed () {
+        return closed;}
   
-    public void reset() {
-        //noop
-    }
-}
+    public void reset () {}
+
+    public boolean isReady () {return true;}
+
+    public void setWriteListener (WriteListener listener) {}}
+
--- src/main/java/org/atomicframework/filterwheel/GZIPResponseWrapper.java
+++ src/main/java/org/atomicframework/filterwheel/GZIPResponseWrapper.java
@@ -1,12 +1,4 @@
-/*
- * Copyright 2003 Jayson Falkner (jayson@jspinsider.com)
- * This code is from "Servlets and JavaServer pages; the J2EE Web Tier",
- * http://www.jspbook.com. You may freely use the code both commercially
- * and non-commercially. If you like the code, please pick up a copy of
- * the book and help support the authors, development of more free code,
- * and the JSP/Servlet/J2EE community.
- */
-package com.jspbook;
+package org.atomicframework.filterwheel;
 
 import java.io.*;
 import java.util.*;
@@ -18,54 +10,33 @@ public class GZIPResponseWrapper extends HttpServletResponseWrapper {
     protected ServletOutputStream stream = null;
     protected PrintWriter writer = null;
 
-    public GZIPResponseWrapper(HttpServletResponse response) {
+    public GZIPResponseWrapper (HttpServletResponse response) {
         super(response);
-        origResponse = response;
-    }
+        origResponse = response;}
 
-    public ServletOutputStream createOutputStream() throws IOException {
-        return (new GZIPResponseStream(origResponse));
-    }
+    public ServletOutputStream createOutputStream () throws IOException {
+        return (new GZIPResponseStream(origResponse));}
 
-    public void finishResponse() {
+    public void finishResponse () {
         try {
-            if (writer != null) {
-                writer.close();
-            } else {
-                if (stream != null) {
-                    stream.close();
-                }
-            }
-        } catch (IOException e) {}
-    }
+            if (writer!=null) {writer.close(); return;}
+            if (stream!=null) {stream.close(); return;}}
+        catch (IOException e) {}}
 
-    public void flushBuffer() throws IOException {
-        stream.flush();
-    }
+    public void flushBuffer () throws IOException {
+        stream.flush();}
 
-    public ServletOutputStream getOutputStream() throws IOException {
-        if (writer != null) {
-            throw new IllegalStateException("getWriter() has already been called!");
-        }
+    public ServletOutputStream getOutputStream () throws IOException {
+        if (writer!=null) throw new IllegalStateException("getWriter() has already been called!");
+        if (stream==null) stream = createOutputStream();
+        return stream;}
 
-        if (stream == null)
-            stream = createOutputStream();
-        return (stream);
-    }
-
-    public PrintWriter getWriter() throws IOException {
-        if (writer != null) {
-            return (writer);
-        }
-
-        if (stream != null) {
-            throw new IllegalStateException("getOutputStream() has already been called!");
-        }
-
+    public PrintWriter getWriter () throws IOException {
+        if (writer!=null) return writer;
+        if (stream!=null) throw new IllegalStateException("getOutputStream() has already been called!");
         stream = createOutputStream();
         writer = new PrintWriter(new OutputStreamWriter(stream, "UTF-8"));
-        return (writer);
-    }
+        return writer;}
 
-    public void setContentLength(int length) {}
-}
+    public void setContentLength(int length) {}}
+
