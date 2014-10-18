9433b6d3098d8b4bc92a93513e4f6f404ffa757d
David A. Ventimiglia <dventimi@gmail.com>
2014-10-18 07:10:21 -0700

    Killing the CacheResponseWrapper as it's now an inner class

--- src/main/java/org/atomicframework/filterwheel/CacheFilter.java
+++ src/main/java/org/atomicframework/filterwheel/CacheFilter.java
@@ -81,4 +81,33 @@ public class CacheFilter extends AbstractHttpFilter {
             this.cache = cache;}
 
         protected OutputStream getBaseStream () {
-            return cache;}}}
+            return cache;}}
+
+    public static class CacheResponseWrapper extends HttpServletResponseWrapper {
+        protected HttpServletResponse origResponse = null;
+        protected ServletOutputStream stream = null;
+        protected PrintWriter writer = null;
+        protected OutputStream cache = null;
+
+        public CacheResponseWrapper (HttpServletResponse response, OutputStream cache) {
+            super(response);
+            origResponse = response;
+            this.cache = cache;}
+
+        public ServletOutputStream createOutputStream () throws IOException {
+            return (new CacheResponseStream(origResponse, cache));}
+
+        public void flushBuffer () throws IOException {
+            stream.flush();}
+
+        public ServletOutputStream getOutputStream () throws IOException {
+            if (writer!=null) throw new IllegalStateException("getWriter() has already been called!");
+            if (stream==null) stream = createOutputStream();
+            return stream;}
+
+        public PrintWriter getWriter () throws IOException {
+            if (writer!=null) return writer;
+            if (stream!=null) throw new IllegalStateException("getOutputStream() has already been called!");
+            stream = createOutputStream();
+            writer = new PrintWriter(new OutputStreamWriter(stream, "UTF-8"));
+            return writer;}}}
