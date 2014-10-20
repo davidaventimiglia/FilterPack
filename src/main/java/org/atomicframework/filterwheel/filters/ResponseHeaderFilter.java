f2504937ae9aed1461ba97a922ed1f8de0aee938
David A. Ventimiglia <dventimi@gmail.com>
2014-10-20 05:15:58 -0700

    Added @Override annotation where appropriate

--- src/main/java/org/atomicframework/filterwheel/filters/ResponseHeaderFilter.java
+++ src/main/java/org/atomicframework/filterwheel/filters/ResponseHeaderFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 import org.atomicframework.filterwheel.api.*;
 
 public class ResponseHeaderFilter extends AbstractHttpFilter {
-    protected void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+    @Override protected void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         for (Enumeration e=fc.getInitParameterNames(); e.hasMoreElements();) {
             String headerName = (String)e.nextElement();
             res.addHeader(headerName, fc.getInitParameter(headerName));}
