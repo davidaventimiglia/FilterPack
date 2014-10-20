30fff88b56b8832218e69ba7b5933ee9984d5f3f
David A. Ventimiglia <dventimi@gmail.com>
2014-10-20 04:43:11 -0700

    Created a ServletOutputStream sub-class that can receive a nested OutputStream to which it will send all its data.

---
+++ src/main/java/org/atomicframework/filterwheel/ComposableServletOutputStream.java
@@ -0,0 +1,23 @@
+package org.atomicframework.filterwheel;
+
+import java.io.*;
+import javax.servlet.*;
+
+public class ComposableServletOutputStream extends ServletOutputStream {
+    protected OutputStream nestedStream = null;
+    public ComposableServletOutputStream (OutputStream stream) {
+        super();
+        if (stream==null) throw new IllegalArgumentException("OutputStream is required.");
+        this.nestedStream = stream;}
+    @Override public void close () throws IOException {
+        nestedStream.close();
+        super.close();}
+    @Override public void flush () throws IOException {
+        nestedStream.flush();}
+    @Override public void write (byte[] b) throws IOException {
+        nestedStream.write(b);}
+    @Override public void write (byte[] b, int off, int len) throws IOException {
+        nestedStream.write(b, off, len);}
+    @Override public void write (int b) throws IOException {
+        nestedStream.write(b);}}
+
