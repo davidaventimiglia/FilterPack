0627d2da4584ed42c30ae4df5d112cc62e09e48d
David A. Ventimiglia <dventimi@gmail.com>
2014-10-20 05:13:50 -0700

    Refactored, stripped out superfluous local booleans, and renamed variables to be more self-descriptive

--- src/main/java/org/atomicframework/filterwheel/filters/GZIPFilter.java
+++ src/main/java/org/atomicframework/filterwheel/filters/GZIPFilter.java
@@ -7,33 +7,33 @@ import javax.servlet.http.*;
 import org.atomicframework.filterwheel.api.*;
 
 public class GZIPFilter extends AbstractHttpFilter {
-    protected void doFilter (HttpServletRequest req, final HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+    @Override protected void doFilter (HttpServletRequest req, final HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         if (req.getHeader("accept-encoding")==null || req.getHeader("accept-encoding").indexOf("gzip")==-1) {chain.doFilter(req, res); return;}
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        GZIPOutputStream deflator = new GZIPOutputStream(bytes);
-        final ComposableServletOutputStream stream = new ComposableServletOutputStream(deflator);
+        ByteArrayOutputStream byteBucket = new ByteArrayOutputStream();
+        GZIPOutputStream deflator = new GZIPOutputStream(byteBucket);
+        final ComposableServletOutputStream bucketStream = new ComposableServletOutputStream(deflator);
         HttpServletResponseWrapper response = new HttpServletResponseWrapper(res) {
-                private PrintWriter writer = null;
-                boolean isOutputStreamReady = false;
-                boolean isWriterReady = false;
+                private PrintWriter myWriter = null;
+                private ServletOutputStream myOutputStream = null;
                 @Override public void flushBuffer () throws IOException {
-                    if (writer!=null) writer.flush(); else stream.flush();}
+                    if (myWriter==null && myOutputStream==null) throw new IllegalStateException("HttpServletResponse is not properly initialized.");
+                    if (myWriter!=null) myWriter.flush();
+                    if (myOutputStream!=null) myOutputStream.flush();}
                 @Override public ServletOutputStream getOutputStream () throws IOException {
-                    if (isOutputStreamReady) throw new IllegalStateException("getOutputStream has already been called.");
-                    if (isWriterReady) throw new IllegalStateException("getWriter has already been called.");
-                    isOutputStreamReady = true;
-                    return stream;}
+                    if (myOutputStream!=null) throw new IllegalStateException("getOutputStream has already been called.");
+                    if (myWriter!=null) throw new IllegalStateException("getWriter has already been called.");
+                    myOutputStream = bucketStream;
+                    return myOutputStream;}
                 @Override public PrintWriter getWriter () throws IOException {
-                    if (isOutputStreamReady) throw new IllegalStateException("getOutputStream has already been called.");
-                    if (isWriterReady) throw new IllegalStateException("getWriter has already been called.");
-                    writer = new PrintWriter(new OutputStreamWriter(stream));
-                    isWriterReady = true;
-                    return writer;}};
+                    if (myOutputStream!=null) throw new IllegalStateException("getOutputStream has already been called.");
+                    if (myWriter!=null) throw new IllegalStateException("getWriter has already been called.");
+                    myWriter = new PrintWriter(new OutputStreamWriter(bucketStream));
+                    return myWriter;}};
         chain.doFilter(req, response);
         response.flushBuffer();
-        stream.flush();
+        bucketStream.flush();
         deflator.finish();
-        byte[] contents = bytes.toByteArray();
+        byte[] contents = byteBucket.toByteArray();
         res.addHeader("Content-Encoding", "gzip");
         res.setContentLength(contents.length);
         res.getOutputStream().write(contents);}}
