57048c92fb18a4f74de4d363564124d87bfcfbf0
David A. Ventimiglia <dventimi@gmail.com>
2014-10-22 04:41:57 -0700

    Renamed some local variables for clarity

--- src/main/java/org/atomicframework/filterwheel/filters/CompressionFilter.java
+++ src/main/java/org/atomicframework/filterwheel/filters/CompressionFilter.java
@@ -7,12 +7,12 @@ import javax.servlet.http.*;
 import org.atomicframework.filterwheel.api.*;
 
 public class CompressionFilter extends AbstractHttpFilter {
-    @Override protected void doFilter (HttpServletRequest req, final HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+    @Override protected void doFilter (HttpServletRequest origReq, final HttpServletResponse origRes, FilterChain chain) throws IOException, ServletException {
         if (req.getHeader("accept-encoding")==null || req.getHeader("accept-encoding").indexOf("gzip")==-1) {chain.doFilter(req, res); return;}
         ByteArrayOutputStream byteBucket = new ByteArrayOutputStream();
         GZIPOutputStream compressor = new GZIPOutputStream(byteBucket);
         final ComposableServletOutputStream bucketStream = new ComposableServletOutputStream(compressor);
-        HttpServletResponseWrapper response = new HttpServletResponseWrapper(res) {
+        HttpServletResponseWrapper newResponse = new HttpServletResponseWrapper(res) {
                 private PrintWriter myWriter = null;
                 private ServletOutputStream myOutputStream = null;
                 @Override public void flushBuffer () throws IOException {
@@ -27,13 +27,13 @@ public class CompressionFilter extends AbstractHttpFilter {
                 @Override public PrintWriter getWriter () throws IOException {
                     if (myOutputStream!=null) throw new IllegalStateException("getOutputStream has already been called.");
                     if (myWriter!=null) throw new IllegalStateException("getWriter has already been called.");
-                    myWriter = new PrintWriter(new OutputStreamWriter(bucketStream));
+                    myWriter = new PrintWriter(new OutputStreamWriter(bucketStream, getCharacterEncoding()));
                     return myWriter;}};
-        chain.doFilter(req, response);
-        response.flushBuffer();
+        chain.doFilter(origReq, newResponse);
+        newResponse.flushBuffer();
         bucketStream.flush();
         compressor.finish();
         byte[] contents = byteBucket.toByteArray();
-        res.addHeader("Content-Encoding", "gzip");
-        res.setContentLength(contents.length);
-        res.getOutputStream().write(contents);}}
+        origRes.addHeader("Content-Encoding", "gzip");
+        origRes.setContentLength(contents.length);
+        origRes.getOutputStream().write(contents);}}
