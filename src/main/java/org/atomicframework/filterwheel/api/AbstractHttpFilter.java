19a9e00c4db8a56e764018f3dd47c4db252be4c0
dventimi <dventimi@workstation>
2014-12-21 13:15:36 -0800

    Restored line break between functions.

--- src/main/java/org/atomicframework/filterwheel/api/AbstractHttpFilter.java
+++ src/main/java/org/atomicframework/filterwheel/api/AbstractHttpFilter.java
@@ -7,12 +7,15 @@ import javax.servlet.http.*;
 public abstract class AbstractHttpFilter implements Filter {
     protected FilterConfig fc;
     protected abstract void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException;
+
     @Override public void doFilter (ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         if (req==null) throw new IllegalArgumentException("HttpServletRequest is required.");
         if (res==null) throw new IllegalArgumentException("HttpServletResponse is required.");
         if (chain==null) throw new IllegalArgumentException("FilterChain is required.");
         if (req instanceof HttpServletRequest) doFilter((HttpServletRequest)req, (HttpServletResponse)res, chain);}
+
     @Override public void init (FilterConfig filterConfig) {
         this.fc = filterConfig;}
+
     @Override public void destroy () {
         this.fc = null;}}
