140bf9637ccdd5678253c2dbf9f909f3af5b6f91
dventimi <dventimi@workstation>
2014-12-21 13:15:51 -0800

    Restored line break between functions.

--- src/main/java/org/atomicframework/filterwheel/api/ComposableServletOutputStream.java
+++ src/main/java/org/atomicframework/filterwheel/api/ComposableServletOutputStream.java
@@ -5,19 +5,25 @@ import javax.servlet.*;
 
 public class ComposableServletOutputStream extends ServletOutputStream {
     protected OutputStream nestedStream = null;
+
     public ComposableServletOutputStream (OutputStream stream) {
         super();
         if (stream==null) throw new IllegalArgumentException("OutputStream is required.");
         this.nestedStream = stream;}
+
     @Override public void close () throws IOException {
         nestedStream.close();
         super.close();}
+
     @Override public void flush () throws IOException {
         nestedStream.flush();}
+
     @Override public void write (byte[] b) throws IOException {
         nestedStream.write(b);}
+
     @Override public void write (byte[] b, int off, int len) throws IOException {
         nestedStream.write(b, off, len);}
+
     @Override public void write (int b) throws IOException {
         nestedStream.write(b);}}
 
