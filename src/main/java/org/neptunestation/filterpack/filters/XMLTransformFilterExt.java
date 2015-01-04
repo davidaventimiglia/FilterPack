443aa6bdca7c566378444fe0c0676b3f06a8814b
David A. Ventimiglia <dventimi@gmail.com>
2015-01-04 08:40:39 -0800

    Added a new version of XMLTransformerFilter (a sub-class) with a more elegant design.

---
+++ src/main/java/org/neptunestation/filterpack/filters/XMLTransformFilterExt.java
@@ -0,0 +1,13 @@
+package org.neptunestation.filterpack.filters;
+
+import java.io.*;
+import java.net.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.neptunestation.filterpack.api.*;
+
+public class XMLTransformFilterExt extends XMLTransformFilter {
+    public static String XSL = "XSL";
+
+    @Override protected void doFilter (HttpServletRequest origReq, HttpServletResponse origRes, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(origReq, new XSLTHttpServletResponse(origRes, getFilterConfig().getInitParameter(XSL)){});}}
