5cd2f8ed1949cf6c18e1319250890a41f5aed2a0
dventimi <dventimi@dventimi-EB1501>
2015-02-09 05:50:17 -0800

    Introducing a new base-class for buffering filters

---
+++ src/main/java/org/neptunestation/filterpack/filters/BufferedFilter.java
@@ -0,0 +1,14 @@
+package org.neptunestation.filterpack.filters;
+
+import java.io.*;
+import java.net.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.neptunestation.filterpack.api.*;
+
+public class BufferedFilter extends AbstractHttpFilter {
+    protected HttpServletResponse wrapResponse (HttpServletResponse origRes) throws ServletException {
+        return new ByteBucketHttpServletResponse(origRes){};}
+
+    @Override protected void doFilter (HttpServletRequest origReq, HttpServletResponse origRes, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(origReq, wrapResponse(origRes));}}
