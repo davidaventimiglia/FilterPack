fd9e76e9670a2615e1bcc673ba1012e5d7dd0de9
dventimi <dventimi@dventimi-EB1501>
2015-02-14 12:52:28 -0800

    Generalized the principle of adding headers into a generic (albeit limited) header injector filter

---
+++ src/main/java/org/neptunestation/filterpack/filters/SimpleHeaderInjectorFilter.java
@@ -0,0 +1,14 @@
+package org.neptunestation.filterpack.filters;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.neptunestation.filterpack.api.*;
+
+public class SimpleHeaderInjectorFilter extends AbstractHttpFilter {
+    @Override protected void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        for (Enumeration e = getFilterConfig().getInitParameterNames(); e.hasMoreElements();) {
+            String name = (String)e.nextElement();
+            res.addHeader(name, getFilterConfig().getInitParameter(name));}
+        chain.doFilter(req, res);}}
