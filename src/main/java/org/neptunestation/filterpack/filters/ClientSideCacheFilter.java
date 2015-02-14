46dfb59b9e8586486c6b498d79d4e1d2cfef3d50
dventimi <dventimi@dventimi-EB1501>
2015-02-14 12:52:02 -0800

    Made the MAX_AGE value configurable via a Filter init parameter

--- src/main/java/org/neptunestation/filterpack/filters/ClientSideCacheFilter.java
+++ src/main/java/org/neptunestation/filterpack/filters/ClientSideCacheFilter.java
@@ -6,6 +6,8 @@ import javax.servlet.http.*;
 import org.neptunestation.filterpack.api.*;
 
 public class ClientSideCacheFilter extends AbstractHttpFilter {
+    public static String MAX_AGE = "MAX_AGE";
+
     @Override protected void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        res.addHeader("Cache-Control", "max-age=3600");
+        res.addHeader("Cache-Control", String.format("max-age=%s", getFilterConfig().getInitParameter(MAX_AGE)));
         chain.doFilter(req, res);}}
