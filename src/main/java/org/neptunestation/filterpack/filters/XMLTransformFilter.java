5f379713ecc4fabd8c8a1c2b7c81996784b93216
dventimi <dventimi@dventimi-EB1501>
2015-02-14 12:50:33 -0800

    Killed off the original XMLTransformFilter and replaced it with what had been XMLTransformFilterExt

--- src/main/java/org/neptunestation/filterpack/filters/XMLTransformFilter.java
+++ src/main/java/org/neptunestation/filterpack/filters/XMLTransformFilter.java
@@ -4,31 +4,13 @@ import java.io.*;
 import java.net.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.xml.transform.*;
-import javax.xml.transform.stream.*;
 import org.neptunestation.filterpack.api.*;
 
-public class XMLTransformFilter extends AbstractHttpFilter {
+public class XMLTransformFilter extends BufferedFilter {
     public static String XSL = "XSL";
 
+    @Override protected HttpServletResponse wrapResponse (HttpServletResponse origRes) throws ServletException {
+        return new XSLTHttpServletResponse(origRes, getFilterConfig().getInitParameter(XSL)){};}
+
     @Override protected void doFilter (HttpServletRequest origReq, HttpServletResponse origRes, FilterChain chain) throws IOException, ServletException {
-        ByteArrayOutputStream target = new ByteArrayOutputStream();
-        BufferedHttpServletResponse newResponse = new BufferedHttpServletResponse(origRes){};
-        chain.doFilter(origReq, newResponse);
-        try {
-            if ((origRes.getContentType()+"").matches(".*/.*\\+?xml.*")) {
-                System.err.println("TRANSFORMING");
-                System.err.println(getFilterConfig().getInitParameter(XSL));
-                TransformerFactory
-                    .newInstance()
-                    .newTransformer(new StreamSource(new StringReader(getFilterConfig().getInitParameter(XSL))))
-                    .transform(new StreamSource(newResponse.getInputStream()), new StreamResult(target));
-                byte[] contents = target.toByteArray();
-                origRes.setContentLength(contents.length);
-                origRes.getOutputStream().write(contents);}
-            else {
-                System.err.println("NOT TRANSFORMING");
-                byte[] contents = newResponse.toByteArray();
-                origRes.setContentLength(contents.length);
-                origRes.getOutputStream().write(contents);}}
-        catch (Throwable t) {t.printStackTrace(System.err);}}}
+        chain.doFilter(origReq, wrapResponse(origRes));}}
