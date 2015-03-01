8dbfe63f3f4752a1a92a04996265346c90ec8f98
dventimi <dventimi@dventimi-EB1501>
2015-03-01 07:33:54 -0800

    Changed the name of the ResponseHeaderFilter to AddResponseHeaderFilter

--- src/main/java/org/neptunestation/filterpack/filters/AddResponseHeaderFilter.java
+++ src/main/java/org/neptunestation/filterpack/filters/AddResponseHeaderFilter.java
@@ -6,7 +6,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import org.neptunestation.filterpack.api.*;
 
-public class ResponseHeaderFilter extends AbstractHttpFilter {
+public class AddResponseHeaderFilter extends AbstractHttpFilter {
     @Override protected void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         for (Enumeration e = getFilterConfig().getInitParameterNames(); e.hasMoreElements();) {
             String name = (String)e.nextElement();
