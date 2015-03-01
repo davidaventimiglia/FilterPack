cb904f1e1b895c5097944e0a654b89d1f3269c53
dventimi <dventimi@dventimi-EB1501>
2015-03-01 07:35:33 -0800

    Added a new filter that can do simple transformations on response headers.

---
+++ src/main/java/org/neptunestation/filterpack/filters/TransformResponseHeaderFilter.java
@@ -0,0 +1,26 @@
+package org.neptunestation.filterpack.filters;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.neptunestation.filterpack.api.*;
+
+public class TransformResponseHeaderFilter extends AbstractHttpResponseFilter {
+    public static String TRIGGER_HEADER = "TRIGGER_HEADER";
+    public static String TRIGGER_HEADER_REGEXP = "TRIGGER_HEADER_REGEXP";
+    public static String TARGET_HEADER_NAME = "TARGET_HEADER_NAME";
+    public static String TARGET_HEADER_VALUE = "TARGET_HEADER_VALUE";
+
+    @Override protected HttpServletResponse wrapResponse (HttpServletResponse origRes) throws ServletException {
+        return new HttpServletResponseWrapper(origRes) {
+            @Override public void addHeader (String name, String value) {
+                if (name!=null && name.equalsIgnoreCase(getFilterConfig().getInitParameter(TARGET_HEADER_NAME))) super.addHeader(getFilterConfig().getInitParameter(TARGET_HEADER_NAME), getFilterConfig().getInitParameter(TARGET_HEADER_VALUE));
+                else super.addHeader(name, value);}
+            @Override public void setHeader (String name, String value) {
+                if (name!=null && name.equalsIgnoreCase(getFilterConfig().getInitParameter(TARGET_HEADER_NAME))) super.setHeader(getFilterConfig().getInitParameter(TARGET_HEADER_NAME), getFilterConfig().getInitParameter(TARGET_HEADER_VALUE));
+                else super.setHeader(name, value);}};}
+
+    @Override protected void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if ((req.getHeader(getFilterConfig().getInitParameter(TRIGGER_HEADER))+"").matches(getFilterConfig().getInitParameter(TRIGGER_HEADER_REGEXP)+"")) chain.doFilter(req, wrapResponse(res));
+        else chain.doFilter(req, res);}}
