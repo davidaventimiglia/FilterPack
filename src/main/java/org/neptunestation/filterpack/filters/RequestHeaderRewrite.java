ad81296cf55fec5bc0a8987dcf2a21e687ea735d
dventimi <dventimi@dventimi-Lenovo-U310>
2015-09-20 14:16:29 -0700

    Working on a POST to PUT rewriter

---
+++ src/main/java/org/neptunestation/filterpack/filters/RequestHeaderRewrite.java
@@ -0,0 +1,16 @@
+package org.neptunestation.filterpack.filters;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.neptunestation.filterpack.api.*;
+
+public class RequestHeaderRewrite extends AbstractHttpFilter {
+    @Override protected void doFilter (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+	if ((""+req.getParameter("method")).equalsIgnoreCase("PUT"))
+	    chain.doFilter(new HttpServletRequestWrapper(req) {@Override public String getMethod () {return "PUT";}},
+			   res);}}
+
+
+
